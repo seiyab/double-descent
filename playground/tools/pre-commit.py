8dddadc9afe31ba882277f3ef2cd420296e3476d
seiya <20365512+seiyab@users.noreply.github.com>
2023-08-21 22:26:38 +0900

    setup pipenv script

--- playground/tools/pre-commit.py
+++ playground/tools/pre-commit.py
@@ -4,13 +4,7 @@ import subprocess
 
 
 def main():
-    black()
-
-
-def black():
-    files = bytes.decode(subprocess.check_output(["git", "ls-files"])).strip().split()
-    py = [f for f in files if f.endswith(".py")]
-    subprocess.check_call(["pipenv", "run", "black", "--check"] + py)
+    subprocess.check_call(["pipenv", "run", "check"])
 
 
 main()
